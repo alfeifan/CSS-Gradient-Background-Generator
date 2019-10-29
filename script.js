6787c38413c306e1329cac48d80c243dd0e98f31
alfeifan <46737644+alfeifan@users.noreply.github.com>
2019-10-29 14:21:24 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector(".button");
+
 
 function setGradient() {
 	body.style.background = 
@@ -14,6 +16,31 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+function colorRan() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+function randomise() {
+	color1.value = colorRan();
+	color2.value = colorRan();
+
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+button.addEventListener("click", randomise);
